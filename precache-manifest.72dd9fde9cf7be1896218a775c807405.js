6601d7bd2a0da66fd974a9fa8766fd4a7189c9ff
selimaakid1 <selima.akid@yahoo.com>
2020-03-02 11:42:07 +0100

    Updates

---
+++ precache-manifest.72dd9fde9cf7be1896218a775c807405.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4e8fec7dac754e8440c1cf96cda826",
+    "url": "/DocplannerReact/index.html"
+  },
+  {
+    "revision": "6b374925f0af5bde1ab0",
+    "url": "/DocplannerReact/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4fdf923e52e1cbd71144",
+    "url": "/DocplannerReact/static/css/main.384f7aa5.chunk.css"
+  },
+  {
+    "revision": "6b374925f0af5bde1ab0",
+    "url": "/DocplannerReact/static/js/2.c8a79899.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/DocplannerReact/static/js/2.c8a79899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fdf923e52e1cbd71144",
+    "url": "/DocplannerReact/static/js/main.543350d5.chunk.js"
+  },
+  {
+    "revision": "00e66c60700c37df5a9d",
+    "url": "/DocplannerReact/static/js/runtime-main.64b469bd.js"
+  }
+]);
